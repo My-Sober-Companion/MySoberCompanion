0de4374fb3415e7de676501bdc6571cd5386a331
Sean Blonien <sean.blonien@parivedasolutions.com>
2020-11-22 14:18:14 -0600

    Made dynamic health metrics

--- components/HealthMetrics.tsx
+++ components/HealthMetrics.tsx
@@ -1,31 +1,57 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
 import { DataTable } from 'react-native-paper';
-import { Text, View } from '../components/Themed';
-import { StyleSheet, Button, Alert, TextInput } from 'react-native';
+import { View } from '../components/Themed';
 
-const HealthMetricsTable = () => (
-  <DataTable>
-    <DataTable.Row>
-      <DataTable.Cell>Pulse</DataTable.Cell>
-      <DataTable.Cell numeric>83 BPM</DataTable.Cell>
-    </DataTable.Row>
+const HealthMetricsTable = () => {
+  const [pulse, setPulse] = useState('83 BPM');
+  const [temp, setTemp] = useState('98.9 F');
 
-    <DataTable.Row>
-      <DataTable.Cell>Sp02</DataTable.Cell>
-      <DataTable.Cell numeric>98%</DataTable.Cell>
-    </DataTable.Row>
+  useEffect(() => {
+    setTimeout(() => {
+      if (pulse === '83 BPM') {
+        setPulse('85 BPM');
+      } else {
+        setPulse('83 BPM');
+      }
+    }, 2000)
+  }, [pulse]);
 
-    <DataTable.Row>
-      <DataTable.Cell>Body Temp</DataTable.Cell>
-      <DataTable.Cell numeric>98.9 F</DataTable.Cell>
-    </DataTable.Row>
+  useEffect(() => {
+    setTimeout(() => {
+      if (temp === '98.9 F') {
+        setTemp('98.7 F');
+      } else {
+        setTemp('98.9 F');
+      }
+    }, 1500)
+  }, [temp]);
 
-    <DataTable.Row>
-      <DataTable.Cell>Breaths PM</DataTable.Cell>
-      <DataTable.Cell numeric>14</DataTable.Cell>
-    </DataTable.Row>
-  </DataTable>
-);
+  return (
+    <DataTable>
+      <DataTable.Row>
+        <DataTable.Cell>Pulse</DataTable.Cell>
+        <DataTable.Cell numeric>{pulse}</DataTable.Cell>
+      </DataTable.Row>
+  
+      <DataTable.Row>
+        <DataTable.Cell>Sp02</DataTable.Cell>
+        <DataTable.Cell numeric>98%</DataTable.Cell>
+      </DataTable.Row>
+  
+      <DataTable.Row>
+        <DataTable.Cell>Body Temp</DataTable.Cell>
+        <DataTable.Cell numeric>{temp}</DataTable.Cell>
+      </DataTable.Row>
+  
+      <DataTable.Row>
+        <DataTable.Cell>Breaths PM</DataTable.Cell>
+        <DataTable.Cell numeric>14</DataTable.Cell>
+      </DataTable.Row>
+    </DataTable>
+  );
+}
 
 export default function HealthMetrics() {
   return (
